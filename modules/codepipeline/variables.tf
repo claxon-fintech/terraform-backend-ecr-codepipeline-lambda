538ff6fa64a1d69d60929a8d85df6a2b32e6f7b0
judahtc <jaycea247@gmail.com>
2025-07-11 08:07:24 +0200

    feat: imported codebuild project name from the codebuild module

--- modules/codepipeline/variables.tf
+++ modules/codepipeline/variables.tf
@@ -16,3 +16,8 @@ variable "github_branch" {
 
 variable "github_oauth_token" {
 }
+
+variable "codebuild_project_name" {
+  type        = string
+  description = "The name of the CodeBuild project to use in CodePipeline"
+}
