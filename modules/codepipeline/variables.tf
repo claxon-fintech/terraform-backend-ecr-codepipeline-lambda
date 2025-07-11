352ea5bbff9d9b31dda0c70a000b1a31f5db1e3a
judahtc <jaycea247@gmail.com>
2025-07-11 07:39:42 +0200

    feat: codepipeline variables

---
+++ modules/codepipeline/variables.tf
@@ -0,0 +1,18 @@
+variable "lambda_function_name" {
+  
+}
+
+variable "aws_region" {
+
+}
+
+variable "FullRepositoryId" {
+
+}
+
+variable "github_branch" {
+
+}
+
+variable "github_oauth_token" {
+}
